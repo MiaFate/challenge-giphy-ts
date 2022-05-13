5c9feee4ef6f2695266c783c2d28f68752354c83
MiaFate <miapurplefroot@gmail.com>
2022-05-12 23:32:32 -0300

    LoginForm ts

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -1,39 +1,32 @@
 import React from 'react';
 import { useNavigate } from "react-router-dom";
 import { VStack, Center, Button, Input, FormControl, FormLabel, FormErrorMessage, FormErrorIcon, InputRightElement } from "@chakra-ui/react";
-import { useForm } from "react-hook-form";
+import { useForm, SubmitHandler, FieldValues } from "react-hook-form";
 import users from "../../bd/users";
 import Cookies from 'universal-cookie';
 
-type username = string | boolean;
-type password = string | boolean;
-
-type data={
-    username: username;
-    password: password;
-}
-interface user{
+type User =  {
     username: string,
     password: string
 }
 
-const LoginForm:React.FC = () => {
+const LoginForm: React.FC = () => {
     const navigate = useNavigate();
-    const { register, handleSubmit, formState: { errors } } = useForm({ defaultValues: { username: false, password: false } });
+    const { register, handleSubmit, formState: { errors } } = useForm<User>();
 
-    const onSubmit = (data:data) => {
+    const onSubmit:SubmitHandler<User> = (data) => {
         const { username, password } = data;
         const isFound = findUser(username, password);
 
         mustRedirect(isFound);
     };
 
-    const findUser = (usuario:username, contraseña:password) => {
-        const userFound = users.find((user:user) => user.username === usuario && user.password === contraseña);
+    const findUser = (usuario: string, contraseña: string) => {
+        const userFound = users.find((user: User) => user.username === usuario && user.password === contraseña);
         return Boolean(userFound);
     };
 
-    const mustRedirect = (found:boolean) => {
+    const mustRedirect = (found: boolean) => {
         if (found) {
             const cookies = new Cookies();
             cookies.set('logged', true, { path: '/', maxAge: 60 });
@@ -48,11 +41,10 @@ const LoginForm:React.FC = () => {
     return (
         <Center w='100vw' h='100vh'>
             <form onSubmit={handleSubmit(onSubmit)}>
-                <VStack textAlign={"center"}  mt={8} spacing="20px">
-                
+                <VStack textAlign={"center"} mt={8} spacing="20px">
                     <FormControl id="usuario" isInvalid={Boolean(errors.username)}>
                         <FormLabel htmlFor="usuario">Usuario</FormLabel>
-                        <Input type="text"  placeholder="Usuario" {...register("username", { required: "Ingrese su nombre de usuario" })} />
+                        <Input type="text" placeholder="Usuario" {...register("username", { required: "Ingrese su nombre de usuario" })} />
                         {errors.username && <> <InputRightElement><FormErrorIcon /></InputRightElement><FormErrorMessage>{errors.username.message}</FormErrorMessage> </>}
                     </FormControl>
                     <FormControl id="contraseña" isInvalid={Boolean(errors.password)}>
